690f39d0a8e730a69973064f9e71e6cd3edcd757
Ian Odhiambo <you@example.com>
2018-10-06 12:00:38 +0300

    added font awsome

--- Instar/settings.py
+++ Instar/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'Instar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'instar',
+        'USER': 'renegade',
+        'PASSWORD': 'instargram',
     }
 }
 
