5e0f06928a38010155b615a823ab415794a4d58c
Ian Odhiambo <you@example.com>
2018-10-09 16:00:29 +0300

    search term

--- social/urls.py
+++ social/urls.py
@@ -4,10 +4,11 @@ from django.conf import settings
 
 # from . import views
 # import welcome from views
-from .views import home, profile
+from .views import home, profile, search
 
 
 urlpatterns = [
     url(r'^$', home, name='home'),
-    url(r'^profile$', profile, name='profile')
+    url(r'^profile$', profile, name='profile'),
+    url(r'^search/', search, name='search_results')
 ]
--- social/views.py
+++ social/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect
+from urllib import request
+
+from .forms import CommentForm, PostForm, ProfileForm
+from .models import Comment, Post, Profile
 from django.contrib.auth.decorators import login_required
-from .models import Post, Profile, Comment
-from .forms import ProfileForm, CommentForm, PostForm
+from django.shortcuts import redirect, render
+
+
 # Create your views here.
 
 
@@ -23,6 +27,7 @@ def home(request):
         form = PostForm()
 
     posts = Post.get_all_posts()
+
     return render(request, 'socials/home.html', {"word": word, "form": form, "posts": posts})
 
 
@@ -46,18 +51,30 @@ def profile(request):
     return render(request, "socials/profile.html", {"profile": profile, "form": form, "posts": posts})
 
 
-def add_photo(request):
-    newit = "This is a model"
+def comment(request):
 
-    # current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload = form.save(commit=False)
+            upload.name = current_user
+            form.save()
+            return redirect('profile')
 
-    # if request.method == 'POST':
-    #     form = ProfileForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         upload = form.save(commit=False)
-    #         upload.User = current_user
-    #         form.save()
-    # else:
+    else:
+        form = ProfileForm()
+    return render(request, "'socials/home.html'", {"newit": newit})
 
-    #     form = ProfileForm()
-    return render(request, "navbar.html", {"newit": newit})
+
+def search(request):
+    if 'profiles' in request.GET and request.GET['profiles']:
+        search_term = request.GET.get('profiles')
+        user = User.objects.filter(username=search_term)
+        Profiles = Profile.objects.filter(username=user)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {"message": message, "profiles": profiles})
+    else:
+        message = "Please search for users"
+
+    return render(request, 'search.html', {"message": message, "profiles": profile})
